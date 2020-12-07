219e8cab0f6ee7b4ce5a282f5649df1f900a574d
Leo veyssiere <lv@homagames.com>
2020-12-07 17:50:01 +0100

    MatchingController : adding tiles with similar Type to the list

--- Assets/_Project/Scripts/MatchingController.cs
+++ Assets/_Project/Scripts/MatchingController.cs
@@ -7,8 +7,10 @@ public class MatchingController : MonoBehaviour
 {
     public LayerMask tileLayer;
     
-    private Tile startTile;
-    private Tile currentTargetedTile;
+    private Tile _startTile;
+    [SerializeField] private List<Tile> _targetedTiles;
+    
+    private RaycastHit _raycastHit;
 
     private Camera _mainCamera;
 
@@ -21,21 +23,38 @@ public class MatchingController : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            RaycastHit hit;
-            
-            if (Physics.Raycast(_mainCamera.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, tileLayer))
+            if (Physics.Raycast(_mainCamera.ScreenPointToRay(Input.mousePosition), out _raycastHit, Mathf.Infinity, tileLayer))
             {
-                Tile hitTile = hit.transform.GetComponent<Tile>();
+                Tile hitTile = _raycastHit.transform.GetComponent<Tile>();
                 
                 if (hitTile != null)
                 {
-                    startTile = hitTile;
+                    _startTile = hitTile;
+                    _targetedTiles.Add(hitTile);
                 }
             }
             else
             {
-                startTile = null;
+                _startTile = null;
             }
+        }
+
+        if (Input.GetMouseButton(0) && _startTile != null)
+        {
+            if (Physics.Raycast(_mainCamera.ScreenPointToRay(Input.mousePosition), out _raycastHit, Mathf.Infinity, tileLayer))
+            {
+                Tile hitTile = _raycastHit.transform.GetComponent<Tile>();
+                
+                if (hitTile != null && hitTile.type == _startTile.type && !_targetedTiles.Contains(hitTile))
+                {
+                    _targetedTiles.Add(hitTile);
+                }
+            }
+        }
+
+        if (Input.GetButtonUp(0)) //Todo : validation de l'action. S'il y a plus d'une tile dans la liste, alors Expand()
+        {
+            _targetedTiles = new List<Tile>();
         }
     }
 }
