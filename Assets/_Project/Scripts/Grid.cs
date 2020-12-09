16e757c4664eb4325939ad9f1f6b20a6bba35425
leov-hg <lv@homagames.com>
2020-12-09 14:30:55 +0100

    Grid system automatisation

---
+++ Assets/_Project/Scripts/Grid.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public int width;
+    public int height;
+
+    [SerializeField] private List<Tile> _tilesList;
+    private Tile[,] _tilesArray;
+
+    public List<Tile> TilesList
+    {
+        get => _tilesList;
+        set => _tilesList = value;
+    }
+
+    public Tile[,] TilesArray
+    {
+        get => _tilesArray;
+        set => _tilesArray = value;
+    }
+
+    private void Awake()
+    {
+        _tilesArray = new Tile[width,height];
+
+        _tilesList = GetComponentsInChildren<Tile>().ToList();
+
+        int index = 0;
+        for (int j = 0; j < height; j++)
+        {
+            for (int i = 0; i < width; i++)
+            {
+                _tilesArray[i, j] = _tilesList[index];
+                index++;
+            }
+        }
+    }
+    
+    
+    public bool AreNeighbors(Tile tile1, Tile tile2)
+    {
+        if (ContainsBoth(tile1, tile2))
+        {
+            int tile1IndexInList = _tilesList.IndexOf(tile1);
+            Vector2 tile1Pos = new Vector2Int(tile1IndexInList % width, Mathf.FloorToInt(tile1IndexInList / width));
+    
+            int tile2IndexInList = _tilesList.IndexOf(tile2);
+            Vector2 tile2Pos = new Vector2Int(tile2IndexInList % width, Mathf.FloorToInt(tile2IndexInList / width));
+
+            return Vector2.Distance(tile1Pos, tile2Pos) == 1; 
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool ContainsBoth(Tile tile1, Tile tile2)
+    {
+        return (_tilesList.Contains(tile1) && _tilesList.Contains(tile2));
+    }
+
+    public bool Contains(Tile tile)
+    {
+        return _tilesList.Contains(tile);
+    }
+}
--- Assets/_Project/Scripts/GridManager.cs
+++ Assets/_Project/Scripts/GridManager.cs
@@ -1,47 +1,21 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using MyBox;
 using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
     public SO_GridManager gridManagerReference;
-    
-    public List<Tile> grid1;
-    public List<Tile> grid2;
-    public int width;
-    public int height;
 
-    private Tile[,] grid1Array;
-    private Tile[,] grid2Array;
+    private List<Grid> _grids;
 
     private void Awake()
     {
         gridManagerReference.gridManager = this;
-        
-        grid1Array = new Tile[width,height];
-        grid2Array = new Tile[width,height];
-        
-        int index = 0;
-        for (int j = 0; j < height; j++)
-        {
-            for (int i = 0; i < width; i++)
-            {
-                grid1Array[i, j] = grid1[index];
-                index++;
-            }
-        }
-        
-        index = 0;
-        for (int j = 0; j < height; j++)
-        {
-            for (int i = 0; i < width; i++)
-            {
-                grid2Array[i, j] = grid2[index];
-                index++;
-            }
-        }
+
+        _grids = GetComponentsInChildren<Grid>().ToList();
     }
 
     private void Start()
@@ -51,107 +25,84 @@ public class GridManager : MonoBehaviour
 
     public bool AreNeighbors(Tile tile1, Tile tile2)
     {
-        if (grid1.Contains(tile1) && grid1.Contains(tile2))
-        {
-            int tile1IndexInList = grid1.IndexOf(tile1);
-            Vector2 tile1Pos = new Vector2Int(tile1IndexInList % width, Mathf.FloorToInt(tile1IndexInList / width));
+        bool res = false;
         
-            int tile2IndexInList = grid1.IndexOf(tile2);
-            Vector2 tile2Pos = new Vector2Int(tile2IndexInList % width, Mathf.FloorToInt(tile2IndexInList / width));
-
-            return Vector2.Distance(tile1Pos, tile2Pos) == 1;
-        }
-        else if (grid2.Contains(tile1) && grid2.Contains(tile2))
+        foreach (Grid grid in _grids)
         {
-            int tile1IndexInList = grid2.IndexOf(tile1);
-            Vector2 tile1Pos = new Vector2Int(tile1IndexInList % width, Mathf.FloorToInt(tile1IndexInList / width));
-        
-            int tile2IndexInList = grid2.IndexOf(tile2);
-            Vector2 tile2Pos = new Vector2Int(tile2IndexInList % width, Mathf.FloorToInt(tile2IndexInList / width));
-
-            return Vector2.Distance(tile1Pos, tile2Pos) == 1;
-        }
-        else
-        {
-            return false;
+            if (grid.AreNeighbors(tile1, tile2))
+            {
+                res = true;
+            }
         }
 
+        return res;
     }
 
     [ButtonMethod]
-    public void Reveal()
+    /*public void Reveal()
     {
         StartCoroutine(RevealCoroutine());
-    }
+    }*/
 
     private IEnumerator RevealLineCoroutine(int lineIndex)
     {
-        for (int i = 0; i < width; i++)
+        foreach (Grid grid in _grids)
         {
-            grid1Array[i,lineIndex].Discover();
-            yield return new WaitForSeconds(0.02f);
+            for (int i = 0; i < grid.width; i++)
+            {
+                grid.TilesArray[i,lineIndex].Discover();
+                yield return new WaitForSeconds(0.02f);
+            }
         }
-        
-        for (int i = 0; i < width; i++)
-        {
-            grid2Array[i,lineIndex].Discover();
-            yield return new WaitForSeconds(0.02f);
-        }
+
     }
 
-    private IEnumerator RevealCoroutine()
+    /*private IEnumerator RevealCoroutine()
     {
         foreach (Tile tile in grid1)
         {
             tile.Discover();
             yield return new WaitForSeconds(0.02f);
         }
-        
-        foreach (Tile tile in grid2)
-        {
-            tile.Discover();
-            yield return new WaitForSeconds(0.02f);
-        }
-    }
+    }*/
 
 
     public void Expand(Tile tileToExpand)
     {
-        List<Tile> localTileList = new List<Tile>();
-        Tile[,] localTileArray = new Tile[,]{};
-        
-        if (grid1.Contains(tileToExpand))
+        Grid localGrid = null;
+
+        foreach (Grid grid in _grids)
         {
-            localTileList = grid1;
-            localTileArray = grid1Array;
-        }
-        else if (grid2.Contains(tileToExpand))
-        {
-            localTileList = grid2;
-            localTileArray = grid2Array;
+            if (grid.Contains(tileToExpand))
+            {
+                localGrid = grid;
+            }
         }
 
-        int tileIndexInList = localTileList.IndexOf(tileToExpand);
-        Vector2Int tilePos = new Vector2Int(tileIndexInList % width, Mathf.FloorToInt(tileIndexInList / width));
-
-        if (tilePos.x - 1 >= 0 && tilePos.y - 1 >= 0 && localTileArray[tilePos.x - 1, tilePos.y - 1].State == Tile.TileState.Hidden)
-            localTileArray[tilePos.x - 1, tilePos.y - 1].Discover();
-        if (tilePos.y - 1 >= 0 && localTileArray[tilePos.x, tilePos.y - 1].State == Tile.TileState.Hidden)
-            localTileArray[tilePos.x , tilePos.y - 1].Discover();
-        if (tilePos.x + 1 <= width - 1 && tilePos.y - 1 >= 0 && localTileArray[tilePos.x + 1, tilePos.y - 1].State == Tile.TileState.Hidden)
-            localTileArray[tilePos.x + 1, tilePos.y - 1].Discover();
+        if (localGrid != null)
+        {
+            int tileIndexInList = localGrid.TilesList.IndexOf(tileToExpand);
+            Vector2Int tilePos = new Vector2Int(tileIndexInList % localGrid.width, Mathf.FloorToInt(tileIndexInList / localGrid.width));
+            
+            if (tilePos.x - 1 >= 0 && tilePos.y - 1 >= 0 && localGrid.TilesArray[tilePos.x - 1, tilePos.y - 1].State == Tile.TileState.Hidden)
+                localGrid.TilesArray[tilePos.x - 1, tilePos.y - 1].Discover();
+            if (tilePos.y - 1 >= 0 && localGrid.TilesArray[tilePos.x, tilePos.y - 1].State == Tile.TileState.Hidden)
+                localGrid.TilesArray[tilePos.x , tilePos.y - 1].Discover();
+            if (tilePos.x + 1 <= localGrid.width - 1 && tilePos.y - 1 >= 0 && localGrid.TilesArray[tilePos.x + 1, tilePos.y - 1].State == Tile.TileState.Hidden)
+                localGrid.TilesArray[tilePos.x + 1, tilePos.y - 1].Discover();
         
-        if (tilePos.x - 1 >= 0 && localTileArray[tilePos.x - 1, tilePos.y].State == Tile.TileState.Hidden)
-            localTileArray[tilePos.x - 1, tilePos.y].Discover();
-        if (tilePos.x + 1 <= width - 1 && localTileArray[tilePos.x + 1, tilePos.y].State == Tile.TileState.Hidden)
-            localTileArray[tilePos.x + 1, tilePos.y].Discover();
+            if (tilePos.x - 1 >= 0 && localGrid.TilesArray[tilePos.x - 1, tilePos.y].State == Tile.TileState.Hidden)
+                localGrid.TilesArray[tilePos.x - 1, tilePos.y].Discover();
+            if (tilePos.x + 1 <= localGrid.width - 1 && localGrid.TilesArray[tilePos.x + 1, tilePos.y].State == Tile.TileState.Hidden)
+                localGrid.TilesArray[tilePos.x + 1, tilePos.y].Discover();
         
-        if (tilePos.x - 1 >= 0 && tilePos.y + 1 <= height - 1 && localTileArray[tilePos.x - 1, tilePos.y + 1].State == Tile.TileState.Hidden)
-            localTileArray[tilePos.x - 1, tilePos.y + 1].Discover();
-        if (tilePos.y + 1 <= height - 1 && localTileArray[tilePos.x, tilePos.y + 1].State == Tile.TileState.Hidden)
-            localTileArray[tilePos.x, tilePos.y + 1].Discover();
-        if (tilePos.x + 1 <= width - 1 && tilePos.y + 1 <= height - 1 && localTileArray[tilePos.x + 1, tilePos.y + 1].State == Tile.TileState.Hidden)
-            localTileArray[tilePos.x + 1, tilePos.y + 1].Discover();
+            if (tilePos.x - 1 >= 0 && tilePos.y + 1 <= localGrid.height - 1 && localGrid.TilesArray[tilePos.x - 1, tilePos.y + 1].State == Tile.TileState.Hidden)
+                localGrid.TilesArray[tilePos.x - 1, tilePos.y + 1].Discover();
+            if (tilePos.y + 1 <= localGrid.height - 1 && localGrid.TilesArray[tilePos.x, tilePos.y + 1].State == Tile.TileState.Hidden)
+                localGrid.TilesArray[tilePos.x, tilePos.y + 1].Discover();
+            if (tilePos.x + 1 <= localGrid.width - 1 && tilePos.y + 1 <= localGrid.height - 1 && localGrid.TilesArray[tilePos.x + 1, tilePos.y + 1].State == Tile.TileState.Hidden)
+                localGrid.TilesArray[tilePos.x + 1, tilePos.y + 1].Discover();
+        }
     }
     
 }
