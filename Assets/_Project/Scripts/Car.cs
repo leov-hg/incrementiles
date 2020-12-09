fdcefc6a975c975d65c8991627841a7edf2dfc35
leov-hg <lv@homagames.com>
2020-12-09 17:47:12 +0100

    Car : MoveTo()

---
+++ Assets/_Project/Scripts/Car.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public float timeTotravel;
+    
+    public void MoveTo(Vector3 pos)
+    {
+        transform.DOMove(pos, timeTotravel);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            MoveTo(transform.position + (Vector3.forward * 2));
+        }
+    }
+}
